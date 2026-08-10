52d3cc39aee2cec51770547e8f1f9362e53f49d4
Tongitaka <epicerietendry@gmail.com>
2026-08-10 05:06:16 +0300

    Add files via upload

---
+++ www/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'tendry-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './html5-qrcode.min.js',
+  './chart.js'
+];
+
+// Fametrahana ireo rakitra ao anaty cache
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Fanadiovana ny cache taloha rehefa misy version vaovao (manova finday/mise à jour)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fampiasana ny cache rehefa offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
